e5830b4855100ce4718ea601b796f8911a7e772b
Lis1us <grandrifter@gmail.com>
2018-01-24 13:33:48 +0300

    Убраны лишние пробелы, переименован ID сессии

--- plugins/mlchatter/mlchatter.py
+++ plugins/mlchatter/mlchatter.py
@@ -2,8 +2,6 @@ import json
 import apiai #используется для подключения к DialogFlow и отправки запроса
 from handler.base_plugin import BasePlugin
 
-
-
 class MLChatterPlugin(BasePlugin):
 
     def __init__(self, prefixes=("",)):
@@ -11,7 +9,6 @@ class MLChatterPlugin(BasePlugin):
         super().__init__()
 
         self.prefixes = prefixes
-
 
     async def check_message(self, msg):
         return not msg.is_out and not msg.is_forwarded
@@ -21,7 +18,7 @@ class MLChatterPlugin(BasePlugin):
             if msg.text.startswith(prefix):
                 request = apiai.ApiAI('ТУТ ВАШ CLIENT ACCESS TOKEN').text_request() # Токен API к Dialogflow
                 request.lang = 'ru' # На каком языке будет послан запрос
-                request.session_id = 'Lis1us_Bot' # ID Сессии диалога (нужно, чтобы потом учить бота)
+                request.session_id = 'Small-Talk' # ID Сессии диалога (нужно, чтобы потом учить бота)
                 request.query = msg.text[len(prefix):].strip() # Посылаем запрос к ИИ с текстом от юзера, при этом убирая префикс
                 responsejson = json.loads(request.getresponse().read().decode('utf-8'))
                 response = responsejson['result']['fulfillment']['speech'] # Разбираем JSON и вытаскиваем ответ
